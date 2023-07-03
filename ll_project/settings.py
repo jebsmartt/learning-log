2fdd3198ef5c7a3c790b96bb6d7a291c3a561ed8
jebsmartt <jeb.smartt@gmail.com>
2023-07-03 10:44:07 -0700

    Require log-in for key features

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # My settings.
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
 
 # Application definition
 
